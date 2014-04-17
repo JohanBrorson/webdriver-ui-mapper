2d6c5a0cbdf0cf033a678babe075b3445ad37213
Johan Brorson <johbro@spotify.com>
2014-04-17 16:04:25 +0200

    Add basic mapping

---
+++ src/main/java/com/github/johanbrorson/uimapper/exceptions/IllegalMethodException.java
@@ -0,0 +1,14 @@
+package com.github.johanbrorson.uimapper.exceptions;
+
+public class IllegalMethodException extends Exception {
+  private static final long serialVersionUID = -8704986557727641732L;
+
+  public IllegalMethodException() {
+    super();
+  }
+
+  public IllegalMethodException(final String message) {
+    super(message);
+  }
+
+}
